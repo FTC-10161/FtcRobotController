18abf3fd532127c2aa907ba9334709c67dead484
various-and-sundry <isaiah@various-and-sundry.com>
2020-12-23 21:42:19 +0000

    Adding "gyroDrive" and "pause" methods to RobotOpMode.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotOpMode.java
@@ -1,11 +1,18 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 public class RobotOpMode extends OpMode {
 
     ULTIMATE_GOAL_HARDWARE_MAP hardware  = new ULTIMATE_GOAL_HARDWARE_MAP();
     ElapsedTime runtime = new ElapsedTime();
+    BNO055IMU imu;
 
     @Override
     public void init() {
@@ -13,10 +20,21 @@ public class RobotOpMode extends OpMode {
     }
 
     @Override
-    public void loop() {
+    public void loop() {    }
 
-    }
 
+
+    //////////////////////////////////////////////////////////// WAIT FUNCTION ////////////////////////////////////////////////////////////
+    public void pause (double duration){
+        while(runtime.milliseconds() < (duration*1000)) {
+            telemetry.addData("Waiting", duration);
+            telemetry.update();
+            }
+        }
+
+
+
+    //////////////////////////////////////////////////////////// DRIVE FUNCTION ////////////////////////////////////////////////////////////
     public void drive(String direction,double speed, double duration){
 
         //Make sure that speed value is a value between 0 and 100 inclusive. If not, say so via telemetry.
@@ -65,6 +83,71 @@ public class RobotOpMode extends OpMode {
             telemetry.update();
         }
 
+        //Stop all motors
+        hardware.frontLeft.setPower(0);
+        hardware.backLeft.setPower(0);
+        hardware.frontRight.setPower(0);
+        hardware.backRight.setPower(0);
+        telemetry.addLine("Robot Stopped");
+        telemetry.update();
+    }
+
+
+
+    //////////////////////////////////////////////////////////// GYRO DRIVE FUNCTION ////////////////////////////////////////////////////////////
+    public void gyroDrive (String direction, double speed, double duration){
+        double heading;
+        double correction;
+        runtime.reset();
+
+        //Make sure that speed value is a value between 0 and 100 inclusive. If not, say so via telemetry.
+        if (speed <= 0 || speed >= 100) {
+            telemetry.addData("Invalid speed value of", speed);
+            telemetry.addLine(". Must be between 0 and 100 inclusive.");
+            telemetry.update();
+        }
+
+        while(runtime.milliseconds() < (duration*1000)) {
+            Orientation angles = this.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+            this.imu.getPosition();
+            heading = angles.firstAngle;
+            correction = heading / 100;
+
+            telemetry.addData("angle", heading);
+            telemetry.update();
+
+            //Drive in the specified direction
+            switch (direction) {
+                case "forward":
+                    hardware.frontLeft.setPower((speed/100) + correction);
+                    hardware.backLeft.setPower((speed/100) + correction);
+                    hardware.frontRight.setPower((speed/100) - correction);
+                    hardware.backRight.setPower((speed/100) - correction);
+                    break;
+                case "backward":
+                    hardware.frontLeft.setPower(-(speed/100) - correction);
+                    hardware.backLeft.setPower(-(speed/100) - correction);
+                    hardware.frontRight.setPower(-(speed/100) + correction);
+                    hardware.backRight.setPower(-(speed/100) + correction);
+                    break;
+                case "rightward":
+                    hardware.frontLeft.setPower((speed/100) + correction);
+                    hardware.backLeft.setPower((-speed/100) - correction);
+                    hardware.frontRight.setPower((-speed/100) + correction);
+                    hardware.backRight.setPower((speed/100) - correction);
+                    break;
+                case "leftward":
+                    hardware.frontLeft.setPower((-speed/100) - correction);
+                    hardware.backLeft.setPower((speed/100) + correction);
+                    hardware.frontRight.setPower((speed/100) - correction);
+                    hardware.backRight.setPower((-speed/100) + correction);
+                    break;
+                default:                                 //If direction is not valid, says so via telemetry.
+                    telemetry.addData("Invalid direction: ", direction, ". Must be forward, backward, leftward, or rightward.");
+                    telemetry.update();
+                    break;
+            }
+        }
         //Stop all motors
         hardware.frontLeft.setPower(0);
         hardware.backLeft.setPower(0);
