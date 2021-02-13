59e4e74f44e7afa7553144dc6d8b6df9bdacba69
FTC-10161 <ftc10161@gmail.com>
2021-02-13 11:13:47 -0500

    Updated Autonomous to use gyroEncoderDrive and also increased Autonomous driving speed.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -11,9 +11,9 @@ public class Autonomous extends RobotOpMode {
 
         hardwareInit();
 
-        gyroTimeDrive("leftward", 35, 4.25);
-        gyroTimeDrive("forward", 35, 0.5);
+        gyroEncoderDrive("leftward", 80, 5.25);
+        gyroEncoderDrive("forward", 80, 0.5);
         pause(1);
-        gyroTimeDrive("rightward", 35, 0.7);
+        gyroEncoderDrive("rightward", 80, 0.7);
     }
 }
