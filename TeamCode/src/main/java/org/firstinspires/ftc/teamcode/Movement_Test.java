7cbc7953361ee60b5bf0f3ded3ad6fc849130c1c
various-and-sundry <isaiah@various-and-sundry.com>
2021-03-07 21:36:32 +0000

    Adding orientation changes to Movement_Test.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Movement_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Movement_Test.java
@@ -12,12 +12,19 @@ public class Movement_Test extends RobotOpMode {
 
         hardwareInit();
 
+        gyroDriveOrientationChange("North");
         gyroEncoderDrive("forward", 35, 4);
         pause(1);
+
+        gyroDriveOrientationChange("East");
         gyroEncoderDrive("rightward", 35, 4);
         pause(1);
+
+        gyroDriveOrientationChange("South");
         gyroEncoderDrive("backward", 35, 4);
         pause(1);
+
+        gyroDriveOrientationChange("West");
         gyroEncoderDrive("leftward", 35, 4);
         pause(1);
     }
