e25a64bb29aeeef96bf07178a40f5ef1cb1f5858
various-and-sundry <isaiah@various-and-sundry.com>
2021-04-11 10:24:19 -0400

    Deleting "Camera_Test" and Updating "Starter_Stack Configuration_Detector"
    The "Starter_Stack_Configuration_Detector" now samples 40 rows on the the Starter Stack rather then 8. It also now compares those rows with other portions of the field rather than a predefined value

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Starter_Stack_Configuration_Detector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Starter_Stack_Configuration_Detector.java
@@ -11,7 +11,7 @@ public class Starter_Stack_Configuration_Detector extends OpenCvPipeline {
     private Mat workingMatrix = new Mat();                  //Matrix to hold input
 
 
-    int rowsContainingRings = 0;
+    int numberOfTimesRingsDetected = 0;
 
     public Starter_Stack_Configuration_Detector() {
     }
@@ -25,22 +25,25 @@ public class Starter_Stack_Configuration_Detector extends OpenCvPipeline {
         }
 
         int rowStart = 130;
-        int rowIncrement = 5;
+        int rowIncrement = 1;
         int colStart = 70;
         int colEnd = 110;
-
+        Mat starterStackRowMatrix;
+        Mat nonStarterStackRowMatrix;
 
         Imgproc.cvtColor(workingMatrix, workingMatrix, Imgproc.COLOR_RGB2HLS);    //Convert image/matrix from RGB to YCrBc.
 
-        rowsContainingRings = 0;
+        numberOfTimesRingsDetected = 0;
 
-        for (int i = 0; i < 8; i++) {
-            Mat starterStackRowMatrix = workingMatrix.submat(i * rowIncrement + rowStart, (i+1) * rowIncrement + rowStart, colStart, colEnd);
+        for (int i = 0; i < 40; i++) {
+            starterStackRowMatrix = workingMatrix.submat(i * rowIncrement + rowStart, (i+1) * rowIncrement + rowStart, colStart, colEnd);
+            nonStarterStackRowMatrix = workingMatrix.submat(i * rowIncrement + rowStart, (i+1) * rowIncrement + rowStart, colStart + 50, colEnd + 50);
 
-            if (Core.sumElems(starterStackRowMatrix).val[2] > 25000)
-                rowsContainingRings++;
+            if (Core.sumElems(starterStackRowMatrix).val[2] > (Core.sumElems(nonStarterStackRowMatrix).val[2] + 10000))
+                numberOfTimesRingsDetected++;
 
             Imgproc.rectangle(workingMatrix, new Rect(colStart, i * rowIncrement + rowStart, colEnd - colStart, rowIncrement), new Scalar(0, 255, 0));
+            Imgproc.rectangle(workingMatrix, new Rect(colStart + 50, i * rowIncrement + rowStart, colEnd - colStart, rowIncrement), new Scalar(0, 255, 0));
         }
 
 
