6a4a35ca5d10e52e6c9dd66bb4b8fa05cb5b1199
various-and-sundry <isaiah@various-and-sundry.com>
2021-05-26 20:50:59 -0400

    Reordering "Two_Wobble_Goal_Autonomous" so that Rings are launch between delivering the two Wobble Goals.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Two_Wobble_Goal_Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Two_Wobble_Goal_Autonomous.java
@@ -26,29 +26,8 @@ public class Two_Wobble_Goal_Autonomous extends RobotOpMode {
             gyroEncoderDrive("forward-left", 80, 1.5);
             gyroEncoderDrive("leftward", 80, 0.75);
             gyroEncoderDrive("rightward", 50, 0.7);
-        }
-        else {
-            gyroEncoderDrive("backward-left", 80, 2);
 
-            if (StarterStackConfiguration == 'A') {
-                gyroEncoderDrive("rightward", 98, 0.7);
-            }
-            else {
-                gyroEncoderDrive("leftward", 80, 3.2);
-                gyroEncoderDrive("rightward", 98, 3);
-            }
-        }
-
-       findAbsolutePosition(0.5,0.25);
-       gyroEncoderDrive("forward", 50 ,2.1);
-
-
-        if(StarterStackConfiguration == 'B') {
-            gyroEncoderDrive("leftward", 80, 1);
-            gyroEncoderDrive("leftward", 80, 2);
-            gyroEncoderDrive("backward-left", 80, 1.75);
-            gyroEncoderDrive("leftward", 80, 1);
-
+            //Launch Rings
             gyroEncoderDrive("rightward", 50, 2.0);
             gyroDriveOrientationChange("East");
             gyroEncoderDrive("leftward", 50, 0.29);
@@ -58,25 +37,59 @@ public class Two_Wobble_Goal_Autonomous extends RobotOpMode {
             gyroEncoderDrive("backward", 80, 1.0);
         }
         else {
+            gyroEncoderDrive("backward-left", 80, 2);
+
+            if (StarterStackConfiguration == 'A') {
+
+                //Launch Rings
+                gyroEncoderDrive("rightward", 80, 1.3);
+                gyroDriveOrientationChange("East");
+                gyroEncoderDrive("leftward", 80, 1.7);
+                launchRings();
+                gyroEncoderDrive("backward", 80, 1.7);
+            }
+            else {
+                gyroEncoderDrive("leftward", 80, 3.2);
+
+                //Launch Rings
+                gyroEncoderDrive("rightward", 100, 4.2);
+                gyroDriveOrientationChange("East");
+                gyroEncoderDrive("leftward", 100, 1.7);
+                launchRings();
+                gyroEncoderDrive("backward", 80, 1.0);
+            }
+        }
+
+        gyroDriveOrientationChange("North");
+        findAbsolutePosition(0.5,0.25);
+        gyroEncoderDrive("forward", 50 ,2.1);
+
+
+        if(StarterStackConfiguration == 'B') {
+            gyroEncoderDrive("leftward", 80, 1);
+            gyroEncoderDrive("leftward", 80, 2);
+            gyroEncoderDrive("backward-left", 100, 1.75);
+            gyroEncoderDrive("leftward", 80, 1);
+
+            //Park on line
+            gyroEncoderDrive("rightward", 100, 0.7);
+        }
+        else {
             gyroEncoderDrive("backward-left", 80, 5);
 
             if (StarterStackConfiguration == 'A') {
                 gyroEncoderDrive("leftward", 80, 1.1);
 
-                gyroEncoderDrive("rightward", 80, 1.3);
-                gyroDriveOrientationChange("East");
-                gyroEncoderDrive("leftward", 50, 1.7);
-                launchRings();
-                gyroEncoderDrive("backward", 80, 1.7);
+                //Park on line
+                gyroEncoderDrive("rightward", 100, 0.7);
+                gyroEncoderDrive("forward", 100, 2);
+                gyroEncoderDrive("leftward", 100, 1.3);
             }
             else {
                 gyroEncoderDrive("leftward", 80, 4.1);
 
-                gyroEncoderDrive("rightward", 80, 4.2);
-                gyroDriveOrientationChange("East");
-                gyroEncoderDrive("leftward", 50, 1.7);
-                launchRings();
-                gyroEncoderDrive("backward", 80, 1.0);
+                //Park on line
+                gyroEncoderDrive("rightward", 100, 2.2);
             }
         }
     }
